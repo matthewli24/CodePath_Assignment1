578f4f71acd1cd73eb9305d9e64d7818e9dec80c
Matthew Li <matthewli2491@gmail.com>
2019-10-25 13:24:21 -0400

    add collection view and configure collection view cell layout

---
+++ flix/assignment1/MovieGridCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCollectionViewCell.swift
+//  assignment1
+//
+//  Created by Super MattMatt on 10/25/19.
+//  Copyright © 2019 assignment1. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var movieImage: UIImageView!
+    
+}
